3e4ba4f0aef7fd10589bc34455cc6c2ad2f9510c
Bogdan Tudose <59750436+dbogt@users.noreply.github.com>
2022-02-20 00:26:10 -0500

    test requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,46 @@
 import streamlit as st
 import pandas as pd
 
+import requests
+
+#%% Test requests
+url = 'https://fred.stlouisfed.org/graph/fredgraph.csv?id=CHXRSA'
+r = requests.get(url)
+open('temp.csv', 'wb').write(r.content)
+df = pd.read_csv('temp.csv')
+st.write("fed csv")
+st.write(df)
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+
+def download_file_from_google_drive(id, destination):
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination) 
+
+
 st.title("Ontario Sunshine List Dashboard")
 st.write("App in progress...")
 st.write("App will load last 10 years of historical public list disclosure from https://www.ontario.ca/page/public-sector-salary-disclosure")
