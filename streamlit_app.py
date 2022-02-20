e4a55de80b2685c827625b7ae3678da3dc9297ab
Bogdan Tudose <59750436+dbogt@users.noreply.github.com>
2022-02-19 22:22:04 -0500

    sp500 test

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,8 @@ import pandas as pd
 st.title("Ontario Sunshine List Dashboard")
 st.write("App in progress...")
 st.write("App will load last 10 years of historical public list disclosure from https://www.ontario.ca/page/public-sector-salary-disclosure")
-csvID = st.secrets['csvID']
+# csvID = st.secrets['csvID']
+csvID = st.secrets['sp500ID']
 path = 'https://drive.google.com/uc?export=download&id='+csvID
 st.write(csvID)
 df = pd.read_csv(path)
