7845af7eece0b4652cdf1ed089631be509ed16c7
Bogdan Tudose <59750436+dbogt@users.noreply.github.com>
2022-02-19 22:18:01 -0500

    write csvid

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,6 @@ st.write("App in progress...")
 st.write("App will load last 10 years of historical public list disclosure from https://www.ontario.ca/page/public-sector-salary-disclosure")
 csvID = st.secrets['csvID']
 path = 'https://drive.google.com/uc?export=download&id='+csvID
+st.write(csvID)
 df = pd.read_csv(path)
 st.write(list(df.columns))
