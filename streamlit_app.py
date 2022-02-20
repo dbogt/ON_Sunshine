ab1ba52048ba0a0a607bdf56fd002849396aca9f
Bogdan Tudose <59750436+dbogt@users.noreply.github.com>
2022-02-19 22:17:27 -0500

    link to google drive

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ import pandas as pd
 st.title("Ontario Sunshine List Dashboard")
 st.write("App in progress...")
 st.write("App will load last 10 years of historical public list disclosure from https://www.ontario.ca/page/public-sector-salary-disclosure")
+csvID = st.secrets['csvID']
+path = 'https://drive.google.com/uc?export=download&id='+csvID
+df = pd.read_csv(path)
+st.write(list(df.columns))
