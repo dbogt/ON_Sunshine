2a0f2375487e7aeef9b6d262e2a582ef21a2dc59
Bogdan Tudose <59750436+dbogt@users.noreply.github.com>
2022-02-19 22:14:01 -0500

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Ontario Sunshine List Dashboard")
+st.write("App in progress...")
+st.write("App will load last 10 years of historical public list disclosure from https://www.ontario.ca/page/public-sector-salary-disclosure")
